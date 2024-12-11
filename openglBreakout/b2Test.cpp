3171d49569bdc29f7cef7349cff1cf799abaeff6
unjerry <2351688512@qq.com>
2024-12-11 20:10:48 +0800

    stage_test_box2dDebugDraw

--- openglBreakout/b2Test.cpp
+++ openglBreakout/b2Test.cpp
@@ -6,10 +6,14 @@
 #include <resource_manager.h>
 #include <learnopengl/filesystem.h>
 #include <sprite_renderer.h>
+#include <math.h>
+#define Min(a, b) ((a < b) ? (a) : (b))
 GladGLContext *gl;
 SpriteRenderer *Rend;
 void DRAWPOLY(b2Transform transform, const b2Vec2 *vertices, int vertexCount, float radius, b2HexColor color, void *context)
 {
+    printf("TRANS (%4.2f,%4.2f) (%4.2f) (%4.2f,%4.2f)\n", vertices[0].x, vertices[0].y, atan2(transform.q.s, transform.q.c), vertices[2].x, vertices[2].y);
+    Rend->DrawSprite(ResourceManager::GetTexture("background"), glm::vec3((vertices[0].x + transform.p.x + (1.0 - transform.q.c + transform.q.s) ) / 30, (vertices[0].y + transform.p.y + (1.0 - transform.q.c - transform.q.s) ) / 30, 0.0f), glm::vec2(2. / 30, 2. / 30), atan2(transform.q.s, transform.q.c) * 180.0 / 2 * M_PI);
 }
 void DRAWSTRING(b2Vec2 p, const char *s, void *context)
 {
@@ -21,7 +25,7 @@ void DrawPolygon(const b2Vec2 *vertices, int vertexCount, b2HexColor color, void
     //     printf("%lf,%lf\n", vertices[i].x, vertices[i].y);
     // }
     // printf("\n");
-    Rend->DrawSprite(ResourceManager::GetTexture("background"), glm::vec3(vertices[0].x / 30, vertices[0].y / 30, 0.0f), glm::vec2((vertices[2].x - vertices[0].x) / 30, (vertices[2].y - vertices[0].y) / 30), 0.0f);
+    Rend->DrawSprite(ResourceManager::GetTexture("particle"), glm::vec3(vertices[0].x / 30, vertices[0].y / 30, 0.0f), glm::vec2((vertices[2].x - vertices[0].x) / 30, (vertices[2].y - vertices[0].y) / 30), 0.0f);
 }
 void DrawCircle(b2Vec2 center, float radius, b2HexColor color, void *context)
 {
@@ -29,12 +33,15 @@ void DrawCircle(b2Vec2 center, float radius, b2HexColor color, void *context)
 }
 void DrawSolidCircle(b2Transform transform, float radius, b2HexColor color, void *context)
 {
+    b2Vec2 center = transform.p;
+    Rend->DrawSprite(ResourceManager::GetTexture("background"), glm::vec3(center.x / 30, center.y / 30, 0.0f), glm::vec2(2 * radius / 30, 2 * radius / 30), 0.0f, glm::vec3(1.0, 0.0, 0.0));
 }
 void DrawSolidCapsule(b2Vec2 p1, b2Vec2 p2, float radius, b2HexColor color, void *context)
 {
 }
 void DrawSegment(b2Vec2 p1, b2Vec2 p2, b2HexColor color, void *context)
 {
+    Rend->DrawSprite(ResourceManager::GetTexture("background"), glm::vec3(Min(p1.x, p2.x) / 30, Min(p1.y, p2.y) / 30, 0.0f), glm::vec2(abs(p1.x - p2.x) / 30, abs(p1.y - p2.y) / 30), 0.0f, glm::vec3(1.0, 0.0, 0.0));
 }
 void DrawTransform(b2Transform transform, void *context)
 {
@@ -94,6 +101,7 @@ int main()
     // ResourceManager::LoadShader(FileSystem::getPath("resources/shaders/particle.vs").c_str(), FileSystem::getPath("resources/shaders/particle.fs").c_str(), nullptr, "particle");
     ResourceManager::LoadShader(FileSystem::getPath("resources/shaders/sprite.vs").c_str(), FileSystem::getPath("resources/shaders/sprite.fs").c_str(), nullptr, "sprite");
     ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/background.jpg").c_str(), false, "background");
+    ResourceManager::LoadTexture(FileSystem::getPath("resources/textures/particle.png").c_str(), true, "particle");
 
     ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
     ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
@@ -126,7 +134,7 @@ int main()
         b2Vec2 position = b2Body_GetPosition(bodyId);
         b2Rot rotation = b2Body_GetRotation(bodyId);
         b2Vec2 velocity = b2Body_GetLinearVelocity(bodyId);
-        printf("t=%4.2f dt=%.6f FPS=%6.0f x(%4.2f %4.2f) v(%4.2f %4.2f)\n", currentFrame, deltaTime, 1.0 / deltaTime, position.x, position.y, velocity.x, velocity.y);
+        // printf("t=%4.2f dt=%.6f FPS=%6.0f x(%4.2f %4.2f) v(%4.2f %4.2f)\n", currentFrame, deltaTime, 1.0 / deltaTime, position.x, position.y, velocity.x, velocity.y);
 
         // render
         // ------
